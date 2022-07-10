e78a38e4446507121d4bf73c4a2c0af84a62da59
zhangchao <529743595@qq.com>
2022-07-10 20:04:54 +0800

    feat: add state

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,35 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import {useState, useContext, createContext} from 'react'
+
+const appContext = createContext<{user: {name: string};setUser: Function;}>(null)
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [user, setUser] = useState({name: 'zch'})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <appContext.Provider value={{user, setUser}}>
+      <div className="App">
+        <FirstChild />
+        <SecondChild />
+        <ThirdChild />
+      </div>
+    </appContext.Provider>
   )
 }
+
+const FirstChild = () => {
+  const {user} = useContext(appContext)
+  return <section>
+    <p>FirstChild</p>
+    <p>{user?.name}</p>
+  </section>
+}
+const SecondChild = () => {
+  const {user, setUser} = useContext(appContext)
+  return <section>
+    <p>SecondChild</p>
+    <input type="text" value={user.name} onChange={(e) => setUser({name: e.target.value})}/>
+  </section>;
+}
+const ThirdChild = () => <section>ThirdChild</section>
 
 export default App
