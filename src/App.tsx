d8e93053f9d570787cc1bc66d48f23ee98a7e2d2
zhangchao <529743595@qq.com>
2022-07-10 20:21:31 +0800

    feat: update reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
 import {useState, useContext, createContext} from 'react'
 
-const appContext = createContext<{user: {name: string};setUser: Function;}>(null)
+type User = {
+  info: {
+    name: string
+  }
+}
+
+const appContext = createContext<{ user: User, setUser: any }>(null)
 
 function App() {
-  const [user, setUser] = useState({name: 'zch'})
+  const [user, setUser] = useState({info: {name: 'zch'}})
   return (
     <appContext.Provider value={{user, setUser}}>
       <div className="App">
@@ -20,14 +26,38 @@ const FirstChild = () => {
   const {user} = useContext(appContext)
   return <section>
     <p>FirstChild</p>
-    <p>{user?.name}</p>
+    <p>{user.info.name}</p>
   </section>
 }
+
+const reducer = (state: User, action: {type: any;value: any;}) => {
+  if (action.type) {
+    if (action.type === 'updateUserName') {
+      console.log({
+        ...state,
+        info: {
+          ...state.info,
+          name: action.value
+        }
+      });
+      return {
+        ...state,
+        info: {
+          ...state.info,
+          name: action.value
+        }
+      }
+    }
+  } else {
+    return state
+  }
+}
+
 const SecondChild = () => {
   const {user, setUser} = useContext(appContext)
   return <section>
     <p>SecondChild</p>
-    <input type="text" value={user.name} onChange={(e) => setUser({name: e.target.value})}/>
+    <input type="text" value={user.info.name} onChange={(e) => setUser(reducer(user, {type: 'updateUserName',value: e.target.value}))}/>
   </section>;
 }
 const ThirdChild = () => <section>ThirdChild</section>
